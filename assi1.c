7ed938a1c579b0cc61574cab6a2f7da19e013bc9
DhruvNarola21 <131172732+DhruvNarola21@users.noreply.github.com>
2023-05-01 08:35:24 +0530

    Add files via upload

---
+++ assi1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+
+	printf("My Name Is:Dhruv\n");
+	printf("My Age Is:18\n");
+	printf("My Birth date Is:1-2-2005\n");
+	printf("My Address Is:Katargam\n");
+	
+	return 0;
+}
